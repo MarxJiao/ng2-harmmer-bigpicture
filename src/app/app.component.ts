a2525850227c57ee55151f2a7502997a19703767
MarxJiao <610529111@qq.com>
2016-10-21 13:39:11 +0800

    change css to less

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Picture } from './PictureData.service.ts';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app works!';
+    pictureData = new Picture();
+    title = 'hallo world';
+    constructor() {
+        console.log(this);
+    }
 }
