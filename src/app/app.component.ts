1ed81c0fe69573bb3f837436ba14c0a2a1a00a06
MarxJiao <610529111@qq.com>
2016-10-22 13:18:40 +0800

    add harmmer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Picture } from './PictureData.service.ts';
-
+// require('harmmerjs');
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+
+    SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
     pictureData = new Picture();
     title = 'hallo world';
     screenHeight = window.screen.height + 'px';
@@ -14,5 +16,8 @@ export class AppComponent {
     ulWidth = window.screen.width * this.pictureData.picArray.length + 'px';
     constructor() {
         console.log(document.body.clientHeight);
+    }
+    log(e) {
+
     }
 }
